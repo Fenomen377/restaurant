f037ce3a5e98e19a8628eaaba67c1cdccb936ddf
Fenomen377 <gkf5153@gmail.com>
2022-11-01 16:21:06 +0300

    create permission classes for views

--- menu/views.py
+++ menu/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from menu.models import *
+from menu.permissions import IsAdminOrReadOnly
 from menu.serializers import *
 
 
@@ -11,11 +12,13 @@ class MenuViewSet(ModelViewSet):
     serializer_class = MenuSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['category', 'price']
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 # mainmenu = [{'title': 'Легенда', 'url_name': 'menu'},
